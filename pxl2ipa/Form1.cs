b9200a7e9fb3675da9e93e5c462fed446f2b11d9
Vaishnav Santhosh <vaihome@hotmail.com>
2021-09-27 22:32:01 +0530

    Added QOL Changes

--- pxl2ipa/Form1.cs
+++ pxl2ipa/Form1.cs
@@ -14,6 +14,16 @@ namespace pxl2ipa
 {
     public partial class Form1 : Form
     {
+        public static string ReplaceLastOccurrence(string Source, string Find, string Replace)
+        {
+            int place = Source.LastIndexOf(Find);
+
+            if (place == -1)
+                return Source;
+
+            string result = Source.Remove(place, Find.Length).Insert(place, Replace);
+            return result;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +33,7 @@ namespace pxl2ipa
         {
             openFileDialog1.ShowDialog();
             textBox1.Text = openFileDialog1.FileName;
+            textBox2.Text = ReplaceLastOccurrence(openFileDialog1.SafeFileName,".pxl",".ipa");
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -33,6 +44,11 @@ namespace pxl2ipa
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if(File.Exists(textBox2.Text))
+            {
+                MessageBox.Show("IPA already exists!", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
             Directory.CreateDirectory(".\\PXL2IPATEMP");
             ZipFile.ExtractToDirectory(textBox1.Text, ".\\PXL2IPATEMP");
             List<string> plist = File.ReadAllLines(".\\PXL2IPATEMP\\PxlPkg.plist").ToList();
@@ -40,6 +56,7 @@ namespace pxl2ipa
             Directory.CreateDirectory(".\\PXL2IPATEMP\\TheIpa\\Payload");
             Directory.Move(".\\PXL2IPATEMP\\app", $".\\PXL2IPATEMP\\TheIpa\\Payload\\{textBox1.Text.Split('\\')[textBox1.Text.Split('\\').Length - 1]}.app");
             ZipFile.CreateFromDirectory(".\\PXL2IPATEMP\\TheIpa", textBox2.Text);
+            Directory.Delete(".\\PXL2IPATEMP", true);
         }
     }
 }
